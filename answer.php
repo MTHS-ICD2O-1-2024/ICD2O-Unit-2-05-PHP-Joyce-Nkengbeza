a1374618319532be857d08e485355f696112c894
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-31 17:24:42 +0000

    changed name

--- answer.php
+++ answer.php
@@ -43,7 +43,7 @@
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/income_tax.jpg" alt="Income Tac Image" />
+        <img src="./images/income_tax.jpg" alt="Income Tax Image" />
       </div>
       <div class="page-content-php">
         <div id="salary">
